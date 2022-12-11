c3e03683c7915857b46cc4cf7dccf24f456a79bb
marsegovia <83015187+marsegovia@users.noreply.github.com>
2022-12-11 12:07:54 -0300

    Add files via upload

---
+++ tarea9.py
@@ -0,0 +1,9 @@
+import time
+
+hora = time.strftime('%H') 
+minutos = time.strftime('%M') 
+
+if int(hora) >= 19:
+	print ("Es hora de irse a casa") 
+else:
+	print ("Quedan {} horas y {} minutos para irnos a casa".format(18- int(hora), 59-int(minutos)))
