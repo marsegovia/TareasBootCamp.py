5b4920f80f355364a43507ef539acb92f62c2399
marsegovia <83015187+marsegovia@users.noreply.github.com>
2022-12-11 11:55:46 -0300

    Add files via upload

---
+++ tarea8/op.py
@@ -0,0 +1,19 @@
+def suma(a, b):
+    return a+b
+
+
+def restar(a, b):
+    return a-b
+
+
+def multi(a, b):
+    return a*b
+ 
+
+def div(a, b):
+    return a/b
+
+
+
+
+    
---
+++ tarea8/tarea8.py
@@ -0,0 +1,16 @@
+import op
+
+Suma = op.suma(4,2)
+Resta = op.restar(4,2)
+Multi = op.multi(4,2)
+Div = op.div(4,2)
+
+print("----Resultados de la importacion------")
+print("Suma:",Suma)
+print("Resta:",Resta)
+print("Multi:",Multi)
+print("Div:", Div)
+
+
+
+
