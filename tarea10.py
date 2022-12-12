33937e5e96e076e19c7d23dda5cdb53cf5d95c12
marsegovia <83015187+marsegovia@users.noreply.github.com>
2022-12-12 19:57:23 -0300

    Update tarea10.py

--- tarea10.py
+++ tarea10.py
@@ -1,4 +1,9 @@
 txt = open("archivo.txt","w")
-texto = txt.write("Creando primer archivo")
+texto = txt.write("Creando primer archivo\n")
 txt.close()
- 
+
+txt = open("archivo.txt","r+")
+txt.readline()
+texto = txt.write("Creando primer archivo\n")
+
+txt.close()
