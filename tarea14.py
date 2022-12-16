e2e97e31d1c44a223cd93d8515b32f6f09a76877
marsegovia <83015187+marsegovia@users.noreply.github.com>
2022-12-16 17:03:11 -0300

    Add files via upload

---
+++ tarea14.py
@@ -0,0 +1,38 @@
+import tkinter
+from tkinter import ttk
+
+
+window = tkinter.Tk()
+def seleccionar1(event):
+    print("Ha seleccionado el boton 1")
+    
+def seleccionar2(event):
+    print("Ha seleccionado el boton 2")
+    
+def seleccionar3(event):
+    print("Ha seleccionado el boton 3")
+    
+def reset(event):
+    print("Reiniciado")
+    return window
+    
+label = tkinter.Label(window, text="Seleccione una opcion:")
+label.grid(column=0, row=0)
+
+selected = tkinter.StringVar()
+
+boton1 = ttk.Radiobutton(window, text="B1", value= '1' ,variable= selected)
+boton2 = ttk.Radiobutton(window, text="B2", value= '2' ,variable= selected)
+boton3 = ttk.Radiobutton(window, text="B3", value= '3' ,variable= selected)
+boton = tkinter.Button(window, text="reset")
+boton1.grid()
+boton2.grid()
+boton3.grid()
+boton.grid(column=0, row=4)
+boton1.bind('<Button-1>', seleccionar1)
+boton2.bind('<Button-1>', seleccionar2)
+boton3.bind('<Button-1>', seleccionar3)
+boton.bind('<Button-1>', reset)
+
+
+window.mainloop()
