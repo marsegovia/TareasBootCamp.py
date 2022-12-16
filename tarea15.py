e64178bb877ae768b7383b0dbb607f019b0ea55b
marsegovia <83015187+marsegovia@users.noreply.github.com>
2022-12-16 18:31:22 -0300

    Update tarea15.py

--- tarea15.py
+++ tarea15.py
@@ -3,14 +3,13 @@ from tkinter import ttk
 
 window = tkinter.Tk()
 
-label = tkinter.Label(window, text="Juegos:")
-label.grid(column=0, row=0)
+label = ttk.Label(text="Juegos:")
+label.pack()
 
 lista = ['GTA V','Worms','CSGO', 'AOE3','ACUnity']
 lista_items = tkinter.StringVar(value=lista)
 
-list_box = tkinter.Listbox(window, height=10, listvariable=lista_items, )
-list_box.grid(column = 0, row=0, sticky= tkinter.W)
-
+list_box = tkinter.Listbox(window, height=10, listvariable=lista_items)
+list_box.pack()
 
 window.mainloop()
